c0dd2ac2677373684c155a43fe220ca0b89f1173
Arjun-R-Dev <34360806+Arjun-R-Dev@users.noreply.github.com>
2018-10-11 01:19:24 +0530

    Create lambdafunction_to_monitor_ec2_snapshots.py

---
+++ lambdafunction_to_monitor_ec2_snapshots.py
@@ -0,0 +1,40 @@
+import boto3
+from botocore.exceptions import ClientError
+from datetime import datetime,timedelta
+import datetime
+import json
+import subprocess
+
+def lambda_handler(event, context):
+    ak=list()
+    aj=list()
+    def test():
+        filters = [{'Name': 'tag-key', 'Values': ['CreatedBy']}]
+        #days = 7
+        #now = datetime.now()
+        ec2 = boto3.client('ec2')
+        regions = ec2.describe_regions().get('Regions',[] )
+        
+        for region in regions:
+            reg=region['RegionName']
+            ec2 = boto3.client('ec2', region_name=reg)
+            result = ec2.describe_volumes( Filters=[{'Name': 'status', 'Values': ['in-use']}])
+        
+            for volume in result['Volumes']:
+                result1 = ec2.describe_snapshots(Filters=filters)
+            
+                for snapshot in result1['Snapshots']:
+                    time = snapshot['StartTime'].replace(tzinfo=None)
+                    print time.date()
+                    if snapshot['VolumeId'] == volume['VolumeId']:
+                        if time.date() > datetime.date.today() - datetime.timedelta(days=7):
+                            
+                            ak.append("snapshot has been taken for the volume %s today and the snapshot ID is : %s "% (volume['VolumeId'] , snapshot['SnapshotId']))
+                            break
+                        else:
+                            aj.append("No backup taken for today for the volume %s  " % (volume['VolumeId']))
+                            
+                            break
+        return (aj,ak)
+    out = test()
+    print (out)
